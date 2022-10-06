084bf2eaf5ecb57bee8658b21a84526e961ef1b1
VladimirSKLV <vladimirsokolov969@gmail.com>
2022-10-06 16:43:28 +0300

    testng

--- src/test/java/ru/netology/service/CashbackHackerTest.java
+++ src/test/java/ru/netology/service/CashbackHackerTest.java
@@ -1,40 +1,45 @@
 package ru.netology.service;
 
-import static org.junit.Assert.*;
+import org.testng.annotations.Test;
+
+import static org.testng.AssertJUnit.assertEquals;
 
 public class CashbackHackerTest {
 
     CashbackHacker service = new CashbackHacker();
 
-    @org.junit.Test
-    public void showBelowBoundary(){
+    @Test
+    public void showBelowBoundary () {
         int purchaseAmount = 600;
         int actualResult = service.remain(purchaseAmount);
         int expectedResult = 400;
         assertEquals(actualResult, expectedResult);
 
     }
-    @org.junit.Test
-    public void showHigherBoundary(){
+
+    @Test
+    public void showHigherBoundary () {
         int purchaseAmount = 1300;
         int actualResult = service.remain(purchaseAmount);
         int expectedResult = 700;
         assertEquals(actualResult, expectedResult);
 
-    }@org.junit.Test
-    public void showNullBoundary(){
+    }
+
+    @Test
+    public void showNullBoundary () {
         int purchaseAmount = 0;
         int actualResult = service.remain(purchaseAmount);
         int expectedResult = 1000;
         assertEquals(actualResult, expectedResult);
 
     }
-    @org.junit.Test
-    public void showEqualBoundary(){
+
+    @Test
+    public void showEqualBoundary () {
         int purchaseAmount = 1000;
         int actualResult = service.remain(purchaseAmount);
-        int expectedResult = 0;
+        int expectedResult = 1000;
         assertEquals(actualResult, expectedResult);
-
     }
 }
